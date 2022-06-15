1af2e3e098b57e2f4c6c11dbc0146e8d7719dc5a
student <i.i.f@mail.ru>
2022-06-16 00:36:15 +0800

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringarraylist/SkyproSpringArraylistApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringarraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringArraylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringArraylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringarraylist/SkyproSpringArraylistApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringarraylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringArraylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
